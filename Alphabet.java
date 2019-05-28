2816ec4ddc462f9e27d60a4359404d4aa989233f
sathyasaranyarv <51054824+sathyasaranyarv@users.noreply.github.com>
2019-05-28 18:22:38 +0530

    Add files via upload

---
+++ Alphabet.java
@@ -0,0 +1,15 @@
+class Alphabet
+{
+public static void main(String args[])
+{
+char c='A';
+if(c>='a' && c<='z')
+{
+System.out.println("A is not an alphabet");
+}
+else if(c>='A' && c<='Z')
+{
+System.out.println("A is an alphabet");
+}
+}
+}
