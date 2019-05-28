937cfd4236fa172be7065f2d51919df05960545a
sathyasaranyarv <51054824+sathyasaranyarv@users.noreply.github.com>
2019-05-28 18:31:41 +0530

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,15 @@
+class EvenOdd
+{
+public static void main(String args[])
+{
+int num=6;
+if(num%2==0)
+{
+System.out.println("Given a number is even");
+}
+else
+{
+System.out.println("Given a number is odd");
+}
+}
+}
